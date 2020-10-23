df89879b9282c2a84b2e009ff0aab7aedb9a7620
David Silva <davidlsilva.sp@gmail.com>
2020-10-23 13:38:39 -0300

    melhoria nos cards

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import './ItemCard.scss';
+import React, { Component } from "react";
+import "./ItemCard.scss";
 class ItemCard extends Component {
-    state = { }
-    render() { 
-        return (
-        <div className="item_card">
-        <img
-          src={this.props.response.attributes.posterImage.tiny}
-          alt=""
-        />
+  state = {};
+  array = this.props.response;
+  render() {
+    return (
+      <div className="item_card">
+        <img src={this.array.attributes.posterImage.tiny} alt="" />
         <div className="row">
-          <h2>{this.props.response.attributes.canonicalTitle}</h2>
-          <span>{this.props.response.attributes.titles.ja_jp}</span>
+          <h2>{this.array.attributes.canonicalTitle}</h2>
+          <span>{this.array.attributes.titles.ja_jp}</span>
           <div className="tags">
-            <span>{this.props.response.attributes.averageRating}</span>
-            <span>{this.props.response.attributes.userCount}</span>
-            <span>{this.props.response.attributes.popularityRank}</span>
-            <span>{this.props.response.attributes.ratingRank}</span>
-            <span>{this.props.response.attributes.status}</span>
+            <span>{this.array.attributes.averageRating}</span>
+            <span>{this.array.attributes.userCount}</span>
+            <span>{this.array.attributes.popularityRank}</span>
+            <span>{this.array.attributes.ratingRank}</span>
+            <span>{this.array.attributes.status}</span>
           </div>
         </div>
       </div>
-       );
-    }
+    );
+  }
 }
- 
+
 export default ItemCard;
