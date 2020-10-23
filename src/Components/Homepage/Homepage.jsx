e645533c3b2e8b244029fc1bc19e22682737598c
David Silva <davidlsilva.sp@gmail.com>
2020-10-23 13:02:05 -0300

    Router

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import TrendingAnimes from '../trendingAnimes';
+import React, { Component } from "react";
+import TrendingAnimes from "../trendingAnimes";
 import "./homepage.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class Homepage extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div id="Homepage">
-                <div className="sidebar"></div>
-                <div className="content">
-                    <TrendingAnimes></TrendingAnimes>
-                </div>
-            </div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <Router>
+        <div id="Homepage">
+          <div className="sidebar">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/TrendingAnimes">TrendingAnimes</Link>
+                </li>
+                <li>
+                  <Link to="/TrendingMangas">TrendingMangas</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route path="/TrendingAnimes">
+                <TrendingAnimes></TrendingAnimes>
+              </Route>
+              <Route path="/TrendingMangas">
+                <TrendingAnimes></TrendingAnimes>
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
- 
+
 export default Homepage;
