577dedc5c6a4e78e4a4a58556d7be1474c305bb6
David Silva <davidlsilva.sp@gmail.com>
2020-10-23 13:56:10 -0300

    Font Awesome e Icones

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faUser, faFireAlt } from "@fortawesome/free-solid-svg-icons";
 import "./ItemCard.scss";
 class ItemCard extends Component {
   state = {};
@@ -11,10 +13,22 @@ class ItemCard extends Component {
           <h2>{this.array.attributes.canonicalTitle}</h2>
           <span>{this.array.attributes.titles.ja_jp}</span>
           <div className="tags">
-            <span>{this.array.attributes.averageRating}</span>
-            <span>{this.array.attributes.userCount}</span>
-            <span>{this.array.attributes.popularityRank}</span>
-            <span>{this.array.attributes.ratingRank}</span>
+            <span className="avaliacoes">
+              <FontAwesomeIcon icon={faStar} />{" "}
+              {Math.round(this.array.attributes.averageRating / 10)}
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faUser} />{" "}
+              {this.array.attributes.userCount}
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faFireAlt} />{" "}
+              {this.array.attributes.popularityRank}
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faStar} />{" "}
+              {this.array.attributes.ratingRank}
+            </span>
             <span>{this.array.attributes.status}</span>
           </div>
         </div>
